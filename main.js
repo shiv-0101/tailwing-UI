78237fa60d826f35a0d4af56659a4f3241527eab
Shivendra Thakur <shivendrathakur321@outlook.com>
2023-04-19 18:20:56 +0530

    One Page with Tailwind UI

---
+++ main.js
@@ -0,0 +1,14 @@
+const mobMenu = document.getElementById('mob-menu')
+const closeBtn = document.getElementById('closebtn')
+const displayOk = document.getElementById('display-ok')
+const openBtn = document.getElementById('openbtn')
+
+closeBtn.addEventListener('click', () => {
+    mobMenu.style.display = 'none'
+    displayOk.style.display= 'none'
+})
+
+openBtn.addEventListener('click', () => {
+    mobMenu.style.display = 'block'
+    displayOk.style.display= 'block'
+})
